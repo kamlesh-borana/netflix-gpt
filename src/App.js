1f3c071fabe409e17c0865a61778133d804b3926
kamlesh-borana <kamlesh.m.borana@gmail.com>
2024-05-12 09:48:58 +0530

    create a store and create and configure userSlice to add user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Provider } from "react-redux";
 import Router from "./router";
+import store from "./redux/store";
 
 function App() {
   return (
     <div className="App">
-      <Router />
+      <Provider store={store}>
+        <Router />
+      </Provider>
     </div>
   );
 }
