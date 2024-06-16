8d33bf8c39ef873dd255dd970bf5424382ae8420
kamlesh-borana <kamlesh.m.borana@gmail.com>
2024-06-16 14:44:05 +0530

    check if user authenticated on browse page logic change

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,18 +3,21 @@ import { auth } from "../utils/firebase";
 import Header from "./Header";
 import { useEffect } from "react";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import { useSelector } from "react-redux";
 
 const Browse = () => {
   const navigate = useNavigate();
 
   useNowPlayingMovies();
 
+  const user = useSelector((store) => store.user);
+
   useEffect(() => {
-    const user = auth.currentUser;
+    // const user = auth.currentUser;
     if (!user) {
       navigate("/");
     }
-  }, []);
+  }, [user]);
 
   return (
     <div>
